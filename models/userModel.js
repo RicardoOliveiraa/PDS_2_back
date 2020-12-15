4ed249f171d55ec0350f3e29fe3a35c5aba99c3f
Ricardo Oliveira <ricardoooliveiraa@outlook.com>
2020-12-15 18:10:07 -0300

    modificações na model de usuario

--- models/userModel.js
+++ models/userModel.js
@@ -1,35 +1,24 @@
 const { Schema } = require("mongoose");
 
 const userModel = Schema({
-    name: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
-    liked_categorys: [
-        {
-            name: { type: String }
-        }
-    ],
     payment_method: { type: String, required: true },
     card_number: { type: String },
-    has_payed: { type: Boolean, default: false},
-    seen_movies: [
+    has_payed: { type: Boolean, default: false },
+    plan: { type: String, required: true },
+    profile_users: [
         {
-            movie: { type: Schema.Types.ObjectId, ref: 'movie'}
-        }
-    ],
-    last_seen_movie_time: { type: Date },
-    plan: { type: String, required: true},
-    sub_user: [
-        {
+            picture: { type: String, required: true },
             name: { type: String, required: true },
-            liked_categorys: [
+            liked_categories: [
                 {
-                    name: { typer: String }
+                    name: { typer: String, required: true }
                 }
             ],
             seen_movies: [
                 {
-                    movie: { type: Schema.Types.ObjectId, ref: 'movie'}
+                    movie: { type: Schema.Types.ObjectId, ref: 'movie' }
                 }
             ],
             last_seen_movie_time: { type: Date },
