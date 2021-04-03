39b1cd09831e8cd4829457a2e172e5d1c42b1f37
Ricardo Oliveira <ricardoooliveiraa@outlook.com>
2021-04-03 13:07:01 -0300

    adição de rotas para realizar update de subusuario
    By Mohamed!

--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,11 +1,9 @@
 const router = express.Router()
 
 const {
-    addUser,
     updateUserSubUsers
 } = require('../../controllers/user')
 
 module.exports =
     router
-        .post('/', addUser)
         .put('/', updateUserSubUsers)
