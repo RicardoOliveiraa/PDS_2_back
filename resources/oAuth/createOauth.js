a29e92040493f62105f42ef883f1986099ced283
Ricardo Oliveira <ricardoooliveiraa@outlook.com>
2021-05-18 20:54:27 -0300

    correções para criação de novo token

--- resources/oAuth/createOauth.js
+++ resources/oAuth/createOauth.js
@@ -3,11 +3,11 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const SCOPES = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = './resources/oAuth/token.json';
 
 // Load client secrets from a local file.
 fs.readFile('./resources/oAuth/credentials.json', (err, content) => {
